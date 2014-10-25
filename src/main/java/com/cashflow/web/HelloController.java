c27ec4d11c31421a6e6019554232a0d8c1bad344
Andreea <andreea.oprisan7@gmail.com>
2014-10-25 23:56:11 +0300

    Initial commit

---
+++ src/main/java/com/cashflow/web/HelloController.java
@@ -0,0 +1,32 @@
+package com.cashflow.web;
+
+import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.sql.DataSource;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    public static final Logger logger = Logger.getLogger(HelloController.class);
+
+    @Autowired
+    DataSource dataSource;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+        DateTime d = new DateTime();
+        logger.debug("hello");
+		model.addAttribute("message", "Hello world!");
+        JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+        jdbc.execute("select * from dual");
+		return "hello";
+	}
+}
